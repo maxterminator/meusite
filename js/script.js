2ca408316b82fa50a914ebb86a7412957f760f34
maxterminator <136817213+maxterminator@users.noreply.github.com>
2023-10-23 21:48:43 -0300

    Aula 7
    Aula 7 ministrada em 23/10/2023.

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// tres constantes para referenciar as "ITEM" divs Projetos
+const form = document.getElementById('formulario');  
+// "const" = Escopo Global - boa pratica é "declar" variavel
+const site = document.getElementById('site');
+const port = document.getElementById('portfolio');
+const movieform = document.querySelector('.movieform');
+const moviesite = document.querySelector('.moviesite');
+const movieport = document.querySelector('.movieport');
+
+
+// evento de click
+form.addEventListener("click",() => {
+    form.classList.toggle('active');
+    movieform.classList.toggle('active');
+});
+site.addEventListener("click",() => {
+    site.classList.toggle('active');
+    moviesite.classList.toggle('active');
+});
+port.addEventListener("click",() => {
+    port.classList.toggle('active');
+    movieport.classList.toggle('active');
+});
